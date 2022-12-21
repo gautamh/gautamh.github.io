130f4a5998fc6d4848f706204a1a1b7aceb4dc49
Gautam Hathi <gautamhathi@live.com>
2022-12-20 22:13:01 -0800

    starting to get a handle on a svelte version of the homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,272 @@
+<!-- +layout.svelte -->
+<script>
+    import { page } from "$app/stores"
+    /**
+	 * @type {string}
+	 */
+     export let href;
+    $: active = $page.url.pathname === href
+
+</script>  
+<header>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163480621-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163480621-1');
+    </script>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Gautam Hathi">
+    <meta name="author" content="Gautam's website">
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <title>Gautam Hathi</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" integrity="sha256-Vzbj7sDDS/woiFS3uNKo8eIuni59rjyNGtXfstRzStA=" crossorigin="anonymous" />
+    <style>
+
+    .jumbotron {
+        height: 100%;
+        max-height: 250;
+    }
+
+    .main-content {
+        margin-top: 30px;
+    }
+
+    body {
+        overflow-x: hidden;
+    }
+
+    .project-img {
+        height: 200px;
+        width: auto;
+        display: block;
+        margin: auto;
+    }
+
+    .thumbnail {
+        overflow: hidden;
+    }
+
+    .navbar {
+        padding-top: 0px;
+        padding-bottom: 0px;
+    }
+
+    .nav-link {
+        padding-top: 4px;
+        padding-bottom: 4px;
+    }
+
+    @media only screen and (max-width: 768px) and (orientation: portrait) {
+        .header-img {
+            width: 75%;
+            height: auto;
+            display: block;
+            margin: auto;
+        }
+
+        .header-left {
+            padding-right: 0px;
+            padding-left: 0px;
+            width: 100vw;
+        }
+
+        .row {
+            margin-left: 0px;
+            margin-right: 0px;
+        }
+    }
+    
+    @media only screen and (min-width: 768px) {
+        .header {
+            max-height: 260px;
+        }
+
+        .header-left {
+            padding: 0px;
+        }
+
+        .header-right {
+            overflow: hidden;
+        }
+
+        .header-img {
+            height: 100%; 
+            width: auto;
+            max-height: 260px;
+        }
+
+        .caption-text {
+            height: 130px;
+        }
+    }
+
+    @media only screen and (max-width: 768px) and (orientation: landscape) {
+        
+        .header-left {
+            padding: 0px;
+            margin: 0px; 
+
+        }
+
+        .header-right {
+            padding: 0px 40px;
+            margin: 0px; 
+        }
+
+        .header-img {
+            width: 75%;
+            height: auto;
+            display: block;
+            margin: auto;
+        }
+
+        .main-content {
+            padding: 0px 40px;
+        }
+
+        .caption-text {
+            height: 175px;
+        }
+    }
+
+    #photo-imgs {
+        /* Prevent vertical gaps */
+        line-height: 0;
+        display: block;
+        z-index: 1;
+        
+        -webkit-column-count: 5;
+        -webkit-column-gap:   8px;
+        -moz-column-count:    5;
+        -moz-column-gap:      0px;
+        column-count:         5;
+        column-gap:           8px;
+        margin-bottom:        25px;
+    }
+
+    #photo-imgs img {
+        /* Just in case there are inline attributes */
+        width: 280px;
+        height: auto !important;
+        
+        margin: 8px 0px;
+        border-width: 20px, 20px, 0px, 0px;
+        
+        transition: all 0.3s;
+        overflow: visible;
+
+    }
+
+    #photo-imgs a {
+        /* Just in case there are inline attributes */
+        width: auto;
+        height: auto !important;
+        overflow: visible;
+    }
+
+    #photo-imgs img:hover {
+        transform: scale(1.03);
+        z-index: 99;
+    }
+
+    @media (max-width: 1750px) {
+        #photo-imgs {
+            -moz-column-count:    4;
+            -webkit-column-count: 4;
+            column-count:         4;
+        }
+    }
+
+    @media (max-width: 1450px) {
+        #photo-imgs {
+            -moz-column-count:    3;
+            -webkit-column-count: 3;
+            column-count:         3;
+        }
+    }
+    @media (max-width: 1050px) {
+        #photo-imgs {
+            -moz-column-count:    2;
+            -webkit-column-count: 2;
+            column-count:         2;
+        }
+    }
+    @media (max-width: 750px) {
+        #photo-imgs {
+            -moz-column-count:    1;
+            -webkit-column-count: 1;
+            column-count:         1;
+        }
+    }
+
+    </style>
+</head>
+</header>
+<body>
+    <main role="main" >
+    <div class="row header">
+        <div class="header-left col-md-10 offset-md-1">
+            <nav class="navbar navbar-expand-lg navbar-light navbar-static-top bg-light">
+                <a class="navbar-brand" href="#"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item {($page.url.pathname === '/')? 'active' : 'inactive'}">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#projects">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#writing">Writing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#photos">Photography</a>
+                        </li>
+                        <li class="nav-item {($page.url.pathname === '/thoughts')? 'active' : 'inactive'}">
+                            <a class="nav-link" href="../../thoughts/index.html">Thoughts</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <h1>{$page.url.pathname}</h1>
+    <slot />
+</main>
+<footer>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" integrity="sha256-yt2kYMy0w8AbtF89WXb2P1rfjcP/HTHLT7097U8Y5b8=" crossorigin="anonymous"></script>
+</footer>
+</body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,190 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+    <script>
+        import Project from './Project.svelte';
+        let projects = [
+        {
+            "title": "ML for data generation",
+            "project_desc": "A paper based on a project I worked on at Google to generate structured data by training Variational Auto Encoders.",
+            "img_src": "alala_path.PNG",
+            "link": "https://arxiv.org/abs/1904.10446"
+        },
+        {
+            "title": "Who The Hill", 
+            "project_desc": "A facial recognition app that allowed reporters to snap a picture of a member of Congress, text it to our service, and get a name back for that member. Co-created as an intern on the New York Times' Interactive News team.",
+            "img_src": "shazongress.png",
+            "link": "https://open.nytimes.com/how-the-new-york-times-uses-software-to-recognize-members-of-congress-29b46dd426c7"
+        },
+        {
+            "title": "FactPopUp",
+            "project_desc": "A Chrome extension used by PolitiFact to deliver live fact checking during the 2016 Presidential Debates. Created for the Duke Reporters' Lab.",
+            "img_src": "factpopup.jpg",
+            "link": "https://www.poynter.org/news/tired-second-screen-tool-will-live-fact-check-debate-you"
+        },
+        {
+            "title": "StoryTracer",
+            "project_desc": "A Chrome extension to identify the source information that a web page is based upon. Created for the Duke Reporters' Lab.",
+            "img_src": "storytracer.png",
+            "link": "https://reporterslab.org/storytracer-google-chrome-extension-original-source/"
+        },
+        {
+            "title": "DSG Voting Networks",
+            "project_desc": "A news article I wrote analyzing voting networks and phantom political parties in Duke's student government.",
+            "img_src": "dsg_voting_networks.png",
+            "link": "https://www.dukechronicle.com/article/2016/05/dsg-records-show-networks-of-voting-patterns"
+        },
+        {
+            "title": "Talking Points",
+            "project_desc": "A data analysis and visualization project that looked at speeches made by members of Congress. This was a class project that used to live on a website but still has interesting components available to look at.",
+            "img_src": "talking_points.PNG",
+            "link": "https://app.powerbi.com/view?r=eyJrIjoiZjJjOWExZjUtYzAwMS00NmJhLWJhNWEtZjZmYjUxYWVjNzY2IiwidCI6IjYxOWUzOWZmLWQ2MTItNDFmZi1iYjEzLTI3Mjk0Nzg2MjQ4MSIsImMiOjN9"
+        },
+        {
+            "title": "Window Seat",
+            "project_desc": "A quick analysis of which side of the plane has the better view.",
+            "img_src": "window_seat.PNG",
+            "link": "https://gautamh.github.io/window-seat/"
+        }
+    ];
+    </script>
+            <div class="row header">
+                <div class="header-left col-md-7 col-sm-12 offset-md-1">
+                    <div class="jumbotron">
+                        <h1>Gautam Hathi</h1>
+                        <hr/>
+                        <h4><i>find me on Twitter <a href="https://twitter.com/gautamhathi">@gautamhathi</a> and on <a href="https://github.com/gautamh">GitHub</a></i></h4>
+                        <h5><i>also maybe try <a rel="me" href="https://mastodon.social/@gautamh">Mastodon</a></i></h5>
+                    </div>
+                </div>
+                <div class="col-md-3 header-right"> 
+                    <img class="header-img" src="../../GKH_FB_4_3.jpg"/>
+                </div>
+                
+            </div>
+            <div class="row main-content">
+                <div class="col-md-10 offset-md-1" id="projects">
+                    <h1>Projects</h1>
+                </div>
+                <div class="row col-md-10 offset-md-1"> 
+                    {#each projects as project, i}
+                        <Project title='{project.title}' project_desc={project.project_desc} img_src={project.img_src} link={project.link}/>
+                    {/each}
+                </div>
+                <div class="col-md-10 offset-md-1" id="writing">
+                    <h1>Writing</h1>
+                </div>
+                <div class="col-md-10 offset-md-1">
+                    <p>See also the <a href="../../thoughts/index.html">Thoughts</a> page.</p>
+                </div>
+                <div class="row col-md-10 offset-md-1">
+                    <div class="col-sm-6 col-md-4">
+                        <blockquote class="blockquote">
+                            <h3>&#39;I&#39;ve not encountered a process...that is less fair&#39;</h3> 
+                            <p class="mb-0">"&#39;I&#39;ve not encountered a process or an administration of the process in any school that is less fair than the undergraduate conduct board hearings that Duke students go through,&#39; said Robert Ekstrand, Law School &#39;98—a Durham lawyer who was involved in the lacrosse case and has worked on hundreds of disciplinary cases involving students at Duke and institutions across the country."</p>
+                            <footer class="blockquote-footer"><cite title="Source Title">Duke Chronicle</cite></footer>
+                            <p><a href="https:&#x2F;&#x2F;www.dukechronicle.com&#x2F;article&#x2F;2017&#x2F;05&#x2F;ive-not-encountered-a-process-that-is-less-fair" class="btn btn-default" role="button">Read...</a></p> 
+                        </blockquote>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <blockquote class="blockquote">
+                            <h3>&#39;A very young person in the White House on a power trip&#39;</h3> 
+                            <p class="mb-0">"Stephen Miller’s path from Duke to Capitol Hill to the White House foreshadowed his actions in the Trump administration."</p>
+                            <footer class="blockquote-footer"><cite title="Source Title">Duke Chronicle</cite></footer>
+                            <p><a href="https:&#x2F;&#x2F;www.dukechronicle.com&#x2F;article&#x2F;2017&#x2F;01&#x2F;very-young-person-in-the-white-house-on-a-power-trip" class="btn btn-default" role="button">Read...</a></p> 
+                        </blockquote>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <blockquote class="blockquote">
+                            <h3>Duke community feels threat from Trump</h3> 
+                            <p class="mb-0">"Although the volatility in the stock market was temporary, the anxiety flowing through campus was more permanent. Student activists who had spent years working for policy goals shared with Hillary Clinton saw their hopes come tumbling down, and students in groups—including Muslims, Latinx, immigrants and women—that had borne the brunt of Donald Trump’s vicious attacks saw him as a personal threat."</p>
+                            <footer class="blockquote-footer"><cite title="Source Title">Duke Chronicle</cite></footer>
+                            <p><a href="https:&#x2F;&#x2F;www.dukechronicle.com&#x2F;article&#x2F;2016&#x2F;11&#x2F;duke-community-feels-threat-from-trump" class="btn btn-default" role="button">Read...</a></p> 
+                        </blockquote>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <blockquote class="blockquote">
+                            <h3>The Rules of the Internet Game</h3> 
+                            <p class="mb-0">"How billions of people around the world interact with, profit from, or get damaged by the internet will be determined by the interactions between the lobbying dollars in Silicon Valey and the egos in Washington DC."</p>
+                            <footer class="blockquote-footer"><cite title="Source Title">Duke Political Review</cite></footer>
+                            <p><a href="https:&#x2F;&#x2F;issuu.com&#x2F;dukedpr&#x2F;docs&#x2F;dpr_2015_spring_web&#x2F;8" class="btn btn-default" role="button">Read...</a></p> 
+                        </blockquote>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <blockquote class="blockquote">
+                            <h3>A Clash of Resurgent Empires</h3> 
+                            <p class="mb-0">"India and China have been fighting over their border for more than fifty years. There may be signs that things are changing, but is either country willing to find an agreement, and could things really be getting worse?"</p>
+                            <footer class="blockquote-footer"><cite title="Source Title">Duke Political Review</cite></footer>
+                            <p><a href="https:&#x2F;&#x2F;issuu.com&#x2F;dukedpr&#x2F;docs&#x2F;dprfall2014_1&#x2F;8" class="btn btn-default" role="button">Read...</a></p> 
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="col-md-10 offset-md-1" id="photos">
+                    <h1>Photography</h1>
+                    <p>In my free time I like to take photos. Here are some samples (click to enlarge and view in a gallery):</p>
+                </div>
+                <div class="row col-md-10 offset-md-1" id="photo-imgs"> 
+                    <a href="photos&#x2F;site_photos-1.jpg" data-fancybox="gallery" data-caption="Sunset over Goat Rocks">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-1.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-2.jpg" data-fancybox="gallery" data-caption="Into the lens">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-2.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-3.jpg" data-fancybox="gallery" data-caption="Looking into the sunset">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-3.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-4.jpg" data-fancybox="gallery" data-caption="San Francisco City Hall">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-4.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-5.jpg" data-fancybox="gallery" data-caption="A tree in snow">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-5.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-6.jpg" data-fancybox="gallery" data-caption="Sihlpromenade, Zürich">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-6.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-35.jpg" data-fancybox="gallery" data-caption="Rearview mirror">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-35.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-8.jpg" data-fancybox="gallery" data-caption="Shadows on the wall">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-8.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-9.jpg" data-fancybox="gallery" data-caption="A swan">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-9.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-10.jpg" data-fancybox="gallery" data-caption="Leavenworth, WA">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-10.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-11.jpg" data-fancybox="gallery" data-caption="Star trails">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-11.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-12.jpg" data-fancybox="gallery" data-caption="A mosque">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-12.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-13.jpg" data-fancybox="gallery" data-caption="Dana, Jordan">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-13.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-14.jpg" data-fancybox="gallery" data-caption="Stars over Wadi Rum">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-14.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-15.jpg" data-fancybox="gallery" data-caption="Grossmünster">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-15.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-16.jpg" data-fancybox="gallery" data-caption="Whirling Dervishes">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-16.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-34.jpg" data-fancybox="gallery" data-caption="Snowfall">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-34.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-36.jpg" data-fancybox="gallery" data-caption="Fall scatterplots">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-36.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-20.jpg" data-fancybox="gallery" data-caption="Market Street">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-20.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-21.jpg" data-fancybox="gallery" data-caption="Sunrise over San Francisco">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-21.jpg" data-holder-rendered="true"> 
+                    </a>
+                    <a href="photos&#x2F;site_photos-25.jpg" data-fancybox="gallery" data-caption="Sunset over the Hudson">
+                        <img class="photo-img" src="../../thumbs&#x2F;site_photos_t-25.jpg" data-holder-rendered="true"> 
+                    </a>
+                </div>
+            </div>
+    
