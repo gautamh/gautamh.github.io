d16582e00f2fb5f67cf95922faacbe5fa5b47d10
Gautam Hathi <gautamhathi@live.com>
2022-12-22 21:18:27 -0800

    move home.scss from static/ to src/lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -46,7 +46,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" integrity="sha256-Vzbj7sDDS/woiFS3uNKo8eIuni59rjyNGtXfstRzStA=" crossorigin="anonymous" />
     <style lang="scss">
-        @import '../static/home.scss';
+        @import '../lib/home.scss';
     </style>
 </svelte:head>
 <body>
