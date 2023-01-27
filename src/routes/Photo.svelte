f771ab1cc478ec126c406998f204d9134c19782c
Gautam Hathi <gautamhathi@live.com>
2023-01-26 22:19:23 -0500

    trying to solve gallery issues (not sure it worked)

--- src/routes/Photo.svelte
+++ src/routes/Photo.svelte
@@ -13,6 +13,6 @@
     export let img_src_thumb;
 </script>
 
-<a href="{img_src}" data-fancybox="gallery" data-caption="{caption}">
+<a href="{img_src}" data-sveltekit-preload-data="off" data-fancybox="gallery" data-caption="{caption}">
     <img class="photo-img" src="{img_src_thumb}" data-holder-rendered="true"> 
 </a>
