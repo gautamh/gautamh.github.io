6fd131fab6477208bfcae281d2ad49710c1b950f
Gautam Hathi <gautamhathi@live.com>
2025-01-04 15:00:56 -0500

    adding llm-fec-scraping to Thoughts

--- src/routes/thoughts/content_utils.js
+++ src/routes/thoughts/content_utils.js
@@ -12,6 +12,10 @@ export const POST_MAP = {
       docId: "1y9aQZRNvJVzKGLuCKFFb3c_HyL6Tmdj2pT38dPXiipA",
       published: true
     },
+    "llm-fec-scraping": {
+      docId: "1TujwPxRbckbKoU43nHNkhwtqk7bFrZYaIVKOoZBaVFU",
+      published: true
+    },
     "example": {
       docId: "1n8yRyoE-64nBhOWUzBOhQ5nPRLnim7f0TI6Yzty-VCI",
       published: false
