6cd196cf6b29dfdd05faf42287b922ce35777291
Gautam Hathi <gautamhathi@live.com>
2025-04-26 15:16:07 -0400

    trying 'rel = "external"' in photo links to see if that makes fancybox work

--- src/routes/Photo.svelte
+++ src/routes/Photo.svelte
@@ -13,6 +13,6 @@
     export let img_src_thumb;
 </script>
 
-<a href="{img_src}" data-sveltekit-preload-data="off" data-fancybox="gallery" data-caption="{caption}">
+<a href="{img_src}" data-sveltekit-preload-data="off" data-fancybox="gallery" data-caption="{caption}" rel="external">
     <img class="photo-img" src="{img_src_thumb}" data-holder-rendered="true"> 
 </a>
